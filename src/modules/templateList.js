eafbcc6d557e2d809b0e66a7873b03c47333fc66
unknown <ekpenisiraphael@gmail.com>
2022-10-16 00:09:44 +0530

    partner code review

---
+++ src/modules/templateList.js
@@ -0,0 +1,8 @@
+export default class ListTemplate {
+    constructor(description, completed, index) {
+      this.description = description;
+      this.completed = completed;
+      this.index = index;
+    }
+  }
+  
